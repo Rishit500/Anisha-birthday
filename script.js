d642a3fd32fe67188b5709814c4fd529255507ae
Rishit500 <hi.iamrishit@gmail.com>
2026-07-05 23:24:36 +0530

    Add loader display logic with a 3-second delay

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Wait 3 seconds
 
+setTimeout(() => {
+
+    document.getElementById("loader").style.display = "none";
+
+    document.getElementById("website").style.display = "block";
+
+}, 3000);
